67531a7174e5712664cca7fdad6ebca7797a1d6b
emmanuel macharia <samuelmarsha@outlook.com>
2025-01-14 00:20:21 +0300

    integrated uploadthing and added file uploads

--- src/app/_components/TopNav.tsx
+++ src/app/_components/TopNav.tsx
@@ -1,8 +1,16 @@
+"use client";
+
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+import { useRouter } from 'next/navigation';
+
 import React from 'react'
+import { UploadButton } from '~/utils/uploadThing'
 
 
 export function TopNav() {
+
+  const router = useRouter();
+
     return(
       <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
         <div className="">Gallery</div>
@@ -11,6 +19,7 @@ export function TopNav() {
             <SignInButton />
           </SignedOut>
           <SignedIn>
+            <UploadButton endpoint="imageUploader"  onClientUploadComplete={() => router.refresh()}/>
             <UserButton />
           </SignedIn>
         </div>
---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,54 @@
+import { auth } from "@clerk/nextjs/server";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import { createUploadthing, type FileRouter } from "uploadthing/next-legacy";
+import { UploadThingError } from "uploadthing/server";
+import { db } from "~/server/db";
+import { images } from "~/server/db/schema";
+
+const f = createUploadthing();
+
+
+// FileRouter for your app, can contain multiple FileRoutes
+export const ourFileRouter = {
+  // Define as many FileRoutes as you like, each with a unique routeSlug
+  imageUploader: f({
+    image: {
+      /**
+       * For full list of options and defaults, see the File Route API reference
+       * @see https://docs.uploadthing.com/file-routes#route-config
+       */
+      maxFileSize: "4MB",
+      maxFileCount: 20,
+    },
+  })
+    // Set permissions and file types for this FileRoute
+    .middleware(async ({ req, res }) => {
+      // This code runs on your server before upload
+      const user = await auth();
+
+      // If you throw, the user will not be able to upload
+      // eslint-disable-next-line @typescript-eslint/only-throw-error
+      if (!user.userId) throw new UploadThingError("Unauthorized");
+
+      // Whatever is returned here is accessible in onUploadComplete as `metadata`
+      return { userId: user.userId };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      // This code RUNS ON YOUR SERVER after upload
+
+      await db.insert(images).values({
+        name: file.name,
+        url: file.url, 
+        userId: metadata.userId
+      })
+      console.log("Upload complete for userId:", metadata.userId);
+
+      console.log("file url", file.url);
+
+      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+      return { uploadedBy: metadata.userId };
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
