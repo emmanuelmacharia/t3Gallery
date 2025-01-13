7e096d2e9789d3680703abd444465da7415852b0
emmanuel macharia <samuelmarsha@outlook.com>
2025-01-13 21:18:20 +0300

    adds the schema and removes mock images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,39 +2,30 @@ import Link from "next/link";
 import Image from "next"
 import { db } from "~/server/db";
 
-const mockUrls = [
-  "https://gmhd64et5c.ufs.sh/f/Q695QrPnWHBTafLdkNyiJ76T2F0IoCV4P3tlbfjHcwyMzrXv",
-  "https://gmhd64et5c.ufs.sh/f/Q695QrPnWHBTe8bi7smkH5YQyOIXB6bfCMZozLNaV94K3p7s",
-  "https://gmhd64et5c.ufs.sh/f/Q695QrPnWHBTzoESlv0ZGIY9fHjDUEBCn75610PblmvSMXoi",
-  "https://gmhd64et5c.ufs.sh/f/Q695QrPnWHBT7Rl3jXcugDvTXWyamYMz6woV5dURxOpZelQk",
-  "https://gmhd64et5c.ufs.sh/f/Q695QrPnWHBTuX67wcetIsvrgnKFV8XqJc4ziQN5jxBEd29h",
-  "https://gmhd64et5c.ufs.sh/f/Q695QrPnWHBTPGIWIZUWMajDgrRmTpO7e8J9IunlL1Ki4A2Q"
-]
+export const dynamic = "force dynamic";
 
-const mockImages = mockUrls.map((url, index) =>( {
-    id: index + 1,
-    url
-}));
 
 export default async function HomePage() {
 
-  const posts = await db.query.posts.findFirst();
+  const images = await db.query.images.findMany(
+    {
+      orderBy: (model, { desc}) => desc(model.id)
+    }
+  );
   
-  console.log(posts);
+  console.log(images);
 
   return (
     <main>
       <div className="flex flex-wrap gap-4">
-        {mockImages.map((image) => (
+        {images.map((image) => (
           <div className="w-48 p-4" key={image.id}>
             <img src={image.url} alt="image" />
+            <div className="text-light">{image.name}</div>
           </div>
         ))
         }
       </div>
-    <div className="w-fit">
-      { posts?.name}
-    </div>
     </main>
   );
 }
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -18,11 +18,12 @@ import {
  */
 export const createTable = pgTableCreator((name) => `t3gallery_${name}`);
 
-export const posts = createTable(
-  "post",
+export const images = createTable(
+  "images",
   {
     id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
-    name: varchar("name", { length: 256 }),
+    name: varchar("name", { length: 256 }).notNull(),
+    url: varchar('url',  { length: 1024}).notNull(),
     createdAt: timestamp("created_at", { withTimezone: true })
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
@@ -34,3 +35,4 @@ export const posts = createTable(
     nameIndex: index("name_idx").on(example.name),
   })
 );
+
